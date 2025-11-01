ec58c141eddcf2eaf367e5a1f6ef0f8207c9a779
Kevson Filipe <kevson@hitapp.com.br>
2025-11-01 11:34:17 -0300

    refactor: atv-1

--- aula-10/atv-1.c
+++ aula-10/atv-1.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-
 int main() {
 	struct Endereco {
 		char logradouro[50];
@@ -20,36 +19,42 @@ int main() {
 	struct Aluno aluno[max_aluno];
 	
 	char menu;
-	int i;
-	for (i = 0; menu != 'S' && i <= (max_aluno - 1); i++) {
-		printf("Digite S para sair ou A para adicionar aluno\n");
+	int i = 0;
+	int max_age = 0;
+	do {
+		printf("Digite S para sair ou A para adicionar aluno: ");
 		scanf(" %c", &menu);
 		
 		if (menu == 'A') {
 			printf("Aluno N: %d \n", i + 1);
-			printf("Nome: \n");
-			scanf("%s", aluno[i].nome);
-			printf("Idade: \n");
+			printf("Nome: ");
+			scanf(" %[^\n]s", aluno[i].nome);
+			printf("Idade: ");
 			scanf("%d", &aluno[i].idade);
-			printf("Logradouro: \n");
-			printf("Numero: \n");
-			printf("CEP: \n");
-			printf("N1: \n");
-			printf("N2: \n");
-			printf("N3: \n");
+			if (aluno[i].idade >= max_age) {
+				max_age = aluno[i].idade;
+			}
+//			printf("Logradouro: \n");
+//			printf("Numero: \n");
+//			scanf("%d", &aluno[i].endereco.numero);
+//			printf("CEP: \n");
+//			printf("N1: \n");
+//			printf("N2: \n");
+//			printf("N3: \n");
 			aluno[i].adicionado = 1;
+			i++;
 		} else {
 			aluno[i].adicionado = 0;
 		}
-	};
+	} while (menu != 'S' && i <= (max_aluno - 1));
 	
+	printf("Maior idade do conjunto %d\n", max_age);
+	printf("============================ Alunos Cadastrados ============================\n");
 	int interable;
 	for(interable = 0; interable <= (max_aluno - 1); interable++) {
-		if (aluno[interable].adicionado == 1) {
+		if (aluno[interable].adicionado == 1 && aluno[interable].idade >= max_age) {
 			printf("Nome: %s\n", aluno[interable].nome);
 			printf("Idade: %d\n", aluno[interable].idade);
-		} else {
-			return 1;
 		}
 	}
 
